0485d47092c43d175ab6d01fb4ca98f7026d73a7
Philipp von Weitershausen <philipp@weitershausen.de>
2005-10-20 13:44:19 +0000

    Use ZPL 2.1 license header instead of ZSVL

--- __init__.py
+++ __init__.py
@@ -1,15 +1,14 @@
 ##############################################################################
 #
-# Copyright (c) 2005 Zope Corporation. All Rights Reserved.
+# Copyright (c) 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
 #
-# This software is subject to the provisions of the Zope Visible Source
-# License, Version 1.0 (ZVSL).  A copy of the ZVSL should accompany this
-# distribution.
-#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE
+# FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
 """Browser Simulator for Functional DocTests
