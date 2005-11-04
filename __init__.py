dc482768c511bf71f530a01f5f2419a477beec0f
Gary Poster <gary@zope.com>
2005-11-04 16:10:27 +0000

    Move the stand-alone modules into testbrowser, leaving the dependency packages out in src still.  This requires the least work, should still support the current zpkg capabilities, and does not require a patch to be sent to the maintainers of the wwwsearch packages.

--- __init__.py
+++ __init__.py
@@ -16,4 +16,33 @@
 $Id$
 """
 
+# TODO We really don't want to be doing these sys.browser hacks.  We're doing
+# them because zpkg doesn't support packaging top-level modules at the moment,
+# and because we have patches, incorporated in these versions, that are not
+# yet part of an official release of the dependencies (although they have been
+# submitted to the maintainer).  It looks likely that 3.2 will ship using these
+# hacks, but they will hopefully be addressed by 3.3.
+import sys
+
+# stitch in ClientForm
+from zope.testbrowser import ClientForm
+
+if 'ClientForm' not in sys.modules:
+    sys.modules['ClientForm'] = ClientForm
+else:
+    assert sys.modules['ClientForm'] is ClientForm
+import ClientForm as x
+assert x is ClientForm
+
+# stitch in pullparser
+from zope.testbrowser import pullparser
+
+if 'pullparser' not in sys.modules:
+    sys.modules['pullparser'] = pullparser
+else:
+    assert sys.modules['pullparser'] is pullparser
+import pullparser as x
+assert x is pullparser
+# end TODO
+
 from testing import Browser
