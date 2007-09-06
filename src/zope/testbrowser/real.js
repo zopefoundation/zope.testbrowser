f837fdb3db92468d2f3a3670ffb668ad6db0ae25
Benji York <benji@benjiyork.com>
2007-09-06 10:50:10 +0000

    move hard-coded file path into test (will eventually be removed alltogether)

--- src/zope/testbrowser/real.js
+++ src/zope/testbrowser/real.js
@@ -110,7 +110,7 @@ function tb_get_link_by_url(url, index) {
         }, index)
 }
 
-function tb_take_screen_shot() {
+function tb_take_screen_shot(out_path) {
     // The `subject` is what we want to take a screen shot of.
     var subject = content.document;
     var canvas = content.document.createElement('canvas');
@@ -119,7 +119,7 @@ function tb_take_screen_shot() {
 
     var ctx = canvas.getContext('2d');
     ctx.drawWindow(content, 0, 0, subject.width, subject.height, 'rgb(0,0,0)');
-    tb_save_canvas(canvas, '/tmp/1.png');
+    tb_save_canvas(canvas, out_path);
 }
 
 function tb_save_canvas(canvas, out_path, overwrite) {
