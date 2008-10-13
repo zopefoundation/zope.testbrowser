114d3f763e9a4584ad3b34b0a968033bc83f0d8e
Wolfgang Schnerring <wosc@wosc.de>
2008-10-13 18:24:07 +0000

    updated version #

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ long_description = (
 
 setup(
     name = 'zope.testbrowser',
-    version = '3.4.3dev',
+    version = '3.4.3',
     url = 'http://pypi.python.org/pypi/zope.testbrowser',
     license = 'ZPL 2.1',
     description = 'Programmable browser for functional black-box tests',
