bfe4d67e4c8e6d1affced966336a1f1d1cfe8b07
Ross Patterson <me@rpatterson.net>
2010-02-20 12:44:17 +0000

    Make the validated HTML available in a response header for inspecting validation problems

--- src/zope/testbrowser/validate.py
+++ src/zope/testbrowser/validate.py
@@ -6,17 +6,19 @@ import urllib2
 class ValidatingHandler(urllib2.BaseHandler):
 
     def http_response(self, request, response):
+        source = response.read()
+        response.seek(0)
         check = urllib2.urlopen(
             os.environ.get('ZOPE_TESTBROWSER_VALIDATE',
                            'http://validator.w3.org/check'),
-            urllib.urlencode(dict(fragment=response.read())))
-        response.seek(0)
+            urllib.urlencode(dict(fragment=source)))
 
         if check.info().get(
             'X-W3C-Validator-Status').lower() != 'valid':
+            hdrs = check.info()
+            hdrs['X-W3C-Validator-Source'] = source
             return self.parent.error(
-                'http', request, check, '500', 'Invalid HTML',
-                check.info())
+                'http', request, check, '500', 'Invalid HTML', hdrs)
             return check
 
         return response
