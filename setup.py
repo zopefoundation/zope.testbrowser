12ccb1d12a02be739a10a2d89484875678d6533f
Michael Howitz <mh@gocept.com>
2010-09-17 18:24:11 +0000

    Back to development: 3.8.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ long_description = (
 
 setup(
     name = 'zope.testbrowser',
-    version='3.8.2',
+    version='3.8.3dev',
     url = 'http://pypi.python.org/pypi/zope.testbrowser',
     license = 'ZPL 2.1',
     description = 'Programmable browser for functional black-box tests',
