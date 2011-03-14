39efae35c2eee43d6d2ac0cffaee6666aa09ec1a
Brian Sutherland <brian@vanguardistas.net>
2011-03-14 08:59:18 +0000

    Finalize release

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ tests_require = ['zope.testing',
 
 setup(
     name = 'zope.testbrowser',
-    version='4.0.0dev',
+    version='4.0.0',
     url = 'http://pypi.python.org/pypi/zope.testbrowser',
     license = 'ZPL 2.1',
     description = 'Programmable browser for functional black-box tests',
