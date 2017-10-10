86ef512c067589b178f9e328379e6f75785dbe02
Marius Gedminas <marius@gedmin.as>
2017-10-10 14:55:23 +0300

    Back to development: 5.2.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ tests_require = ['zope.testing', 'mock']
 
 setup(
     name='zope.testbrowser',
-    version='5.2.2',
+    version='5.2.3.dev0',
     url='https://github.com/zopefoundation/zope.testbrowser',
     license='ZPL 2.1',
     description='Programmable browser for functional black-box tests',
